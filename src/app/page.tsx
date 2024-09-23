ad2618fd59a18517c9c569f34bc6b7e4320bd5be
Matheus Silva <msilvaqs@outlook.com>
2024-09-23 09:03:03 -0400

    Fixing errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,14 +5,12 @@ interface HomeProps {
   searchParams: { genre: string };
 }
 
-const API_KEY = process.env.API_KEY;
-
 const Home: React.FC<HomeProps> = async ({ searchParams }) => {
   const genre = searchParams.genre || "fetchTrending";
   const res = await fetch(
     `https://api.themoviedb.org/3${
       genre === "fetchTopRated" ? `/movie/top_rated` : `/trending/all/week`
-    }?api_key=${API_KEY}&language=en-US&page=1`,
+    }?api_key=${process.env.API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 10000 } }
   );
 
